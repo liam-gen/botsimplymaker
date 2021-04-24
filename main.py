614f9dae221822d2d7b74b6d747584cb33ecb97d
liam-gen <74251382+liam-gen@users.noreply.github.com>
2021-04-24 14:36:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from code import *
 
-token = input('token : ')
-## Commande ping
+token = 'VOTRE_TOKEN_ICI'
+
+## Exemple commande ping :
 command('!ping', ":ping_pong: Pong ! Je t'ai répondu en `$ping` millisecondes")
-## Commandes infos
+
+## Exemple commandes infos
 command('!bot-info', "Le ping du bot est de `$ping` ms !\nJ'ai $commandCount commandes\nJe suis sur $serversCount serveurs\nUtilisateurs : $usersCount\nVersion discord : $version")
 command('!user-info', "Ton nom est $username, ton id est $authorID")
-command('!test', "$mentioned")
+
+## Change le status :
 setstatus('!help')
+
+## Connexion au bot
 login(token)
